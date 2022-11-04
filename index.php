26ed1dab610d55157134c694bd6d168d0862ed9e
Jp-Pacheco <joao2524244@rede.ulbra.br>
2022-11-03 22:01:53 -0300

    calc_das_boas

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+ <head>
+  <title>Calc Média</title>
+ </head>
+ <body>
+ <form action='media.php' method="get">
+  Nota 1: <input type="text" name="num1"><br>
+  Nota 2: <input type="text" name="num2"><br>
+  Nota 3: <input type="text" name="num3"><br>
+  <input type="submit">
+ </form>
+ </body>
+</html>
